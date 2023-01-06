394920bba87396428149ee664976c4fec159df8a
Konrad Miekus <miekus.konrad@gmail.com>
2023-01-06 07:30:50 +0100

    use typescript in nextauth routes

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,17 +5,23 @@ export const authOptions = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
-      async authorize(credentials, req) {
+      credentials: {
+        username: { type: 'text' },
+        password: { type: 'text' }
+      },
+      async authorize(credentials, _req) {
+        if (!credentials) return null
+
         const { username, password } = credentials
 
-        if (username === 'konrad' && password === '123') return { username: 'konrad', name: 'Konrad' }
+        if (username === 'konrad' && password === '123') return { username: 'konrad', name: 'Konrad', id: '1' }
         return null
       }
     })
   ],
-  session: {
-    strategy: 'jwt',
-  },
+  // session: {
+  //   strategy: 'jwt',  // it's a default strategy, but for some reason Typescript has a problem with that
+  // },
   pages: {
     signIn: '/auth/login',
   }
