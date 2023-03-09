366dc3831de7598fbd4d64b8c5aae7b07be02386
Konrad Miekus <miekus.konrad@gmail.com>
2023-03-09 07:20:22 +0100

    Render the application in electron using loadUrl

---
+++ electron/index.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron')
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      contextIsolation: true
+    }
+  })
+  // win.loadFile('./index.html')
+  win.loadURL('http://localhost:3000')
+}
+app.whenReady().then(createWindow)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
