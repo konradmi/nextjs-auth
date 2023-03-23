1d8ed074523536c94adc06c42c828751a4ebdfb2
Konrad Miekus <miekus.konrad@gmail.com>
2023-03-23 06:59:31 +0100

    protect /admin and /user routes

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,25 @@
-export { default } from 'next-auth/middleware'
+import { withAuth } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
+
+export default withAuth(
+  function middleware(req: any) {
+    if (req.nextUrl.pathname.startsWith('/admin') && !req.nextauth.token.user.features.includes('admin')) {
+      return NextResponse.redirect(
+        new URL('/auth/login?message=NotAdmin', req.url)
+      )
+    }
+    if (req.nextUrl.pathname.startsWith('/user') && !req.nextauth.token.user.features.includes('user')) {
+      return NextResponse.redirect(
+        new URL('/auth/login?message=NotAdmin', req.url)
+      )
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }: any) => !!token
+    },
+  }
+)
 
 export const config = {
   matcher: ['/admin', '/user']
